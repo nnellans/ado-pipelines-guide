18b4734a568a362922d2b34debf43e9050f21e8c
Nathan Nellans <54514037+nnellans@users.noreply.github.com>
2026-01-04 13:31:48 -0600

    Add service principal support for Azure Pipelines agent

--- self-hosted-agents-docker/linux/start.sh
+++ self-hosted-agents-docker/linux/start.sh
@@ -8,6 +8,14 @@ if [ -z "${AZP_URL}" ]; then
   exit 1
 fi
 
+if [ -n "$AZP_CLIENTID" ]; then
+  echo "Using service principal credentials to get token"
+  az login --allow-no-subscriptions --service-principal --username "$AZP_CLIENTID" --password "$AZP_CLIENTSECRET" --tenant "$AZP_TENANTID"
+  # adapted from https://learn.microsoft.com/en-us/azure/databricks/dev-tools/user-aad-token
+  AZP_TOKEN=$(az account get-access-token --query accessToken --output tsv)
+  echo "Token retrieved"
+fi
+
 if [ -z "${AZP_TOKEN_FILE}" ]; then
   if [ -z "${AZP_TOKEN}" ]; then
     echo 1>&2 "error: missing AZP_TOKEN environment variable"
@@ -18,6 +26,7 @@ if [ -z "${AZP_TOKEN_FILE}" ]; then
   echo -n "${AZP_TOKEN}" > "${AZP_TOKEN_FILE}"
 fi
 
+unset AZP_CLIENTSECRET
 unset AZP_TOKEN
 
 if [ -n "${AZP_WORK}" ]; then
@@ -54,6 +63,47 @@ print_header "1. Determining matching Azure Pipelines agent..."
 
 AZP_AGENT_PACKAGES=$(curl -LsS \
     -u user:$(cat "${AZP_TOKEN_FILE}") \
+    -H "Accept:application/json" \
+    "${AZP_URL}/_apis/distributedtask/packages/agent?platform=${TARGETARCH}&top=1")
+
+AZP_AGENT_PACKAGE_LATEST_URL=$(echo "${AZP_AGENT_PACKAGES}" | jq -r ".value[0].downloadUrl")
+
+if [ -z "${AZP_AGENT_PACKAGE_LATEST_URL}" -o "${AZP_AGENT_PACKAGE_LATEST_URL}" == "null" ]; then
+  echo 1>&2 "error: could not determine a matching Azure Pipelines agent"
+  echo 1>&2 "check that account "${AZP_URL}" is correct and the token is valid for that account"
+  exit 1
+fi
+
+print_header "2. Downloading and extracting Azure Pipelines agent..."
+
+curl -LsS "${AZP_AGENT_PACKAGE_LATEST_URL}" | tar -xz & wait $!
+
+source ./env.sh
+
+trap "cleanup; exit 0" EXIT
+trap "cleanup; exit 130" INT
+trap "cleanup; exit 143" TERM
+
+print_header "3. Configuring Azure Pipelines agent..."
+
+# Despite it saying "PAT", it can be the token through the service principal
+./config.sh --unattended \
+  --agent "${AZP_AGENT_NAME:-$(hostname)}" \
+  --url "${AZP_URL}" \
+  --auth "PAT" \
+  --token $(cat "${AZP_TOKEN_FILE}") \
+  --pool "${AZP_POOL:-Default}" \
+  --work "${AZP_WORK:-_work}" \
+  --replace \
+  --acceptTeeEula & wait $!
+
+print_header "4. Running Azure Pipelines agent..."
+
+chmod +x ./run.sh
+
+# To be aware of TERM and INT signals call ./run.sh
+# Running it with the --once flag at the end will shut down the agent after the build is executed
+./run.sh "$@" & wait $!    -u user:$(cat "${AZP_TOKEN_FILE}") \
     -H "Accept:application/json;" \
     "${AZP_URL}/_apis/distributedtask/packages/agent?platform=${TARGETARCH}&top=1")
 
